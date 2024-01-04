ae8333b18cb87c87987bbd094c05091400d36731
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-01-04 14:11:49 -0800

    Side Menu Styling
    styling

--- src/SideMenu.tsx
+++ src/SideMenu.tsx
@@ -3,10 +3,18 @@ import React from "react";
 function SideMenu() {
   return (
     <div className="small-menubar">
-      <div className="menu-item">A</div>
-      <div className="menu-item">B</div>
-      <div className="menu-item">C</div>
-      <div className="menu-item">D</div>
+      <div className="menu-item">
+        <span className="material-symbols-outlined">gallery_thumbnail</span>
+      </div>
+      <div className="menu-item">
+        <span className="material-symbols-outlined">monetization_on</span>
+      </div>
+      <div className="menu-item">
+        <span className="material-symbols-outlined">mail</span>
+      </div>
+      <div className="menu-item">
+        <span className="material-symbols-outlined">photo_camera_front</span>{" "}
+      </div>
     </div>
     // <div id="portfolioMenu">
     //   <label>Portfolios</label>
