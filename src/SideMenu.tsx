d2c2150f03a5c185ec87ca125205d4f62bb7cd61
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-01-05 14:33:39 -0800

    Styling
    styling

--- src/SideMenu.tsx
+++ src/SideMenu.tsx
@@ -6,21 +6,25 @@ function SideMenu() {
       <div className="menu-item">
         <a href="">
           <span className="material-symbols-outlined">gallery_thumbnail</span>
+          <span className="menu-item-label">Portfolios</span>
         </a>
       </div>
       <div className="menu-item">
         <a href="">
           <span className="material-symbols-outlined">monetization_on</span>
+          <span className="menu-item-label">Pricings</span>
         </a>
       </div>
       <div className="menu-item">
         <a href="">
           <span className="material-symbols-outlined">mail</span>
+          <span className="menu-item-label">Contact</span>
         </a>
       </div>
       <div className="menu-item">
         <a href="">
           <span className="material-symbols-outlined">photo_camera_front</span>
+          <span className="menu-item-label">About Me</span>
         </a>
       </div>
     </div>
