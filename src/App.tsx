69d87d28127779d272aaa37b8498dbc8ef26a4f5
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-01-05 21:01:50 -0800

    More Styling
    styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="wrapper">
-      {/* <Header></Header> */}
+      <Header></Header>
       <Content></Content>
       {/* <Footer></Footer> */}
     </div>
