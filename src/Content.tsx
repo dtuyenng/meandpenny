95e0b1bb9f17008fe4b27d3c8b56bbb1b04eb882
dtuyenng <136381756+dtuyenng@users.noreply.github.com>
2024-01-09 20:10:48 -0800

    More components
    components

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SideMenu from "./SideMenu";
 import Portfolio from "./Portfolio";
+import AboutMe from "./components/AboutMe";
 
 function Content() {
   return (
     <div id="content">
       <SideMenu />
-      <Portfolio />
+      {/* <Portfolio /> */}
+      <AboutMe></AboutMe>
     </div>
   );
 }
